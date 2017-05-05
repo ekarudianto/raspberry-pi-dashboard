b9cbd1745a9391ce13efed183732bb10641ecd6a
Eka Rudianto <ekarudianto@yahoo.com>
2017-05-06 02:32:39 +0800

    Refactoring

--- gulp/watch.js
+++ gulp/watch.js
@@ -5,17 +5,18 @@ import LOGGER from 'gulp-util';
 
 const app = () => {
   return gulp.watch(GulpConfig.WATCH_LIST.APP, ['copy-base-files', 'reload',])
-  .on('change', (e) => {
-    LOGGER.log(LOGGER.colors.bgYellow(e.path + ' has changed, reloading...'));
-  });
+    .on('change', (e) => {
+      LOGGER.log(LOGGER.colors.bgYellow(e.path + ' has changed, reloading...'));
+    });
 };
 
 const dist = () => {
   return gulp.watch(GulpConfig.WATCH_LIST.DIST)
-  .on('change', (e) => {
-    LOGGER.log(LOGGER.colors.bgYellow(e.path + ' has changed, reloading...'));
-    gulp.src(['./' + GulpConfig.DIST + '/**',]).pipe(connect.reload());
-  });
+    .on('change', (e) => {
+      LOGGER.log(LOGGER.colors.bgYellow(e.path + ' has changed, reloading...'));
+
+      gulp.src(['./' + GulpConfig.DIST + '/**',]).pipe(connect.reload());
+    });
 };
 
 
